e1a3c8b9ce202650f2f548e4b4d7c42214a772fe
Mike <michaelcarrick1234@gmail.com>
2024-08-09 17:08:39 +0100

    uploading first time

--- bulletbounce.py
+++ bulletbounce.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import time
 
 # Initialize Pygame
 pygame.init()
@@ -17,6 +18,17 @@ WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BG = (0, 0, 0)
 BLUE = (0, 0, 255)
+
+
+
+players = [
+    {"x": 50, "y": 450,  "gun_x": 1, "gun_y": 0, "colour": RED, "js_num": 0, "gravity": 0.1, "y_speed": 0,
+     "fire_rate": 200, "reload_speed": 1000, "time_last_shot_fired": 0, "max_bullets": 5, "current_bullets": 5, "is_reloading": False,
+    "time_start_reload": 0},
+    {"x": 500, "y": 450,  "gun_x": -1, "gun_y": 0, "colour": BLUE, "js_num": 1, "gravity": 0.1, "y_speed": 0,
+     "fire_rate": 100, "reload_speed": 1000, "time_last_shot_fired": 0, "max_bullets": 5, "current_bullets": 5, "is_reloading": False,
+    "time_start_reload": 0}
+]
 
 # Box coordinates and dimensions
 
@@ -44,6 +56,7 @@ gun_x = 300
 gun_y = 500
 
 
+
 # Game loop
 running = True
 while running:
@@ -61,8 +74,6 @@ while running:
 
         bullet["x"] += bullet["x_speed"]
         bullet["y"] += bullet["y_speed"]
-
-
 
         # NEED TO ADJUST FOR CENTRE OF BULLET
         for box in boxes:
@@ -99,35 +110,58 @@ while running:
                     bullet["y_speed"] = -bullet["bounce"] * bullet["y_speed"]
                     bullet["bounce_count"] += 1
                     bullet["y"] = box["y"] + box["height"]
+    # rotate player guns
+    for p in players:
+        joystick = joysticks[p["js_num"]]
+        right_analog_x = joystick.get_axis(2)
+        right_analog_y = joystick.get_axis(3)
+        left_analog_x = joystick.get_axis(0)
+        left_analog_y = joystick.get_axis(1)
+        a = joystick.get_button(0)
 
-    joystick = joysticks[0]
-    right_analog_x = joystick.get_axis(2)
-    right_analog_y = joystick.get_axis(3)
-    left_analog_x = joystick.get_axis(0)
-    left_analog_y = joystick.get_axis(1)
+        # have we avoided drift?
+
+        if abs(right_analog_x) + abs(right_analog_y) > 0.1:
+            angle_radians = math.atan2(right_analog_y, right_analog_x)
+            angle_degrees = math.degrees(angle_radians)
+            p["gun_x"] = math.cos(angle_radians)
+            p["gun_y"] = math.sin(angle_radians)
+
+        if abs(left_analog_x) + abs(left_analog_y) > 0.1:
+            p["x"] += left_analog_x * 2.5
+
+        # jumped?
+        if a:
+            p["y_speed"] = -2
+
+        # gravity pull on players
+        p["y_speed"] += p["gravity"]
+        p["y"] += p["y_speed"]
+        p["y"] = min(p["y"], screen_height - 14)
+        p["y"] = max(p["y"],14 )
+        p["x"] = max(14, p["x"])
+        p["x"] = min(screen_width - 14, p["x"])
 
 
-    # have we avoided drift?
-    if abs(right_analog_x) + abs(right_analog_y) > 0.1:
-        angle_radians = math.atan2(right_analog_y, right_analog_x)
-        angle_degrees = math.degrees(angle_radians)
-        print ("x=", right_analog_x, " y=", right_analog_y)
-        print("angle degrees = ", angle_degrees)
-        gun_angle_x = math.cos(angle_radians)
-        gun_angle_y = math.sin(angle_radians)
+        # create new bullet
+        if joystick.get_axis(5) > -1 and p["current_bullets"] > 0 and pygame.time.get_ticks() >= p["time_last_shot_fired"] + p["fire_rate"] and p["is_reloading"] == False:
 
-    if abs(left_analog_x) + abs(left_analog_y) > 0.1:
-        gun_x += left_analog_x * 2.5
-        gun_y += left_analog_y * 2.5
-    gun_y += gravity
+            bullets = bullets + [ {"x": p["x"] + 20 * p["gun_x"], "y": p["y"] + 20 * p["gun_y"], "x_speed": p["gun_x"] * 20, "y_speed": p["gun_y"] * 20, "gravity": 0.1, "bounce_count": 10, "active": 1,
+                 "bounce": 0.3}
+            ]
+            p["current_bullets"] -= 1
+            p["time_last_shot_fired"] = pygame.time.get_ticks()
 
+        # time to reload?
+        if p["is_reloading"] == False:
+            if p["current_bullets"] == 0 or joystick.get_axis(4) > -1:
+                p["is_reloading"] = True
+                p["time_start_reload"] = pygame.time.get_ticks()
 
-    # if a pressed create new bullet
-    if joystick.get_axis(5) > -1:
-
-        bullets = bullets + [ {"x": gun_x + 20 * gun_angle_x, "y": gun_y + 20 * gun_angle_y, "x_speed": gun_angle_x * 20, "y_speed": gun_angle_y * 20, "gravity": 0.1, "bounce_count": 10, "active": 1,
-             "bounce": 0.3}
-        ]
+        # reloading complete?
+        if p["is_reloading"] and pygame.time.get_ticks() >= p["time_start_reload"] + p["reload_speed"]:
+            p["current_bullets"] = p["max_bullets"]
+            p["is_reloading"] = False
 
     # Draw everything
     screen.fill(BG)
@@ -137,14 +171,18 @@ while running:
         if bullet["active"] == 0:
             bullets.remove(bullet)
         pygame.draw.circle(screen, BLUE, (bullet["x"], bullet["y"]), 5)
-        pygame.draw.rect(screen, BLUE, (gun_x-14, gun_y-14,28,28))
-    pygame.draw.line(screen, (255, 255, 255), (gun_x, gun_y), (gun_x + 20 * gun_angle_x, gun_y + 20 * gun_angle_y))
+
+    # draw players
+    for p in players:
+        pygame.draw.rect(screen, p["colour"], (p["x"]-14, p["y"]-14,28,28))
+        pygame.draw.line(screen, (255, 255, 255), (p["x"], p["y"]), (p["x"] + 20 * p["gun_x"], p["y"] + 20 * p["gun_y"]))
     pygame.display.flip()
 
 
 
     # Cap the frame rate
     pygame.time.Clock().tick(60)
+
 
 # Quit Pygame
 pygame.quit()
